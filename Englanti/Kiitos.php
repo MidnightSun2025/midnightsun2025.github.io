228c61fe691e47f77acaf2a326ed1f626f1e5c63
MidnightSun2025 <mmidnightsun2025@gmail.com>
2025-01-28 16:49:50 +0200

    Create Kiitos.php

---
+++ Englanti/Kiitos.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+<title>Mensa Midnight Sun 2017</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" type="text/css" href="../style.css" />
+<!-- <link rel="shortcut icon" href="../favicon.ico" /> -->
+</head>
+<body>
+<!--
+<div id="header">
+<!-- <a href="../index.php"><img src="../images/midsun-logo.png" alt="Logo" /></a>  -->
+</div>
+
+
+<div id="navigation">
+	<ul>
+		<li><a href="Ohjelma.php" id="ohjelma">Program</a></li>
+		<li><a href="Ilmoittautuminen.php" id="ilmoittautuminen">Registration</a></li>
+		<!--<li><a href="Yhteystiedot.php" id="yhteystiedot">Contacts</a></li>-->
+	</ul>
+</div><body id="Ilmoittautuminen">
+<div id="container">
+<div id="content">
+<div id="contentleft">
+
+<center><br/>Thank you for your registration
+<br><a href="../index.html">Home
+</center>
+
+</div>
+
+<div id="sidebar">
+<div align="right"><!--
+<form name="form1">
+<select style="font-size:12px;color:#006699;font-family:verdana;background-color:#ffffff;" name="menu" onChange="location=document.form1.menu.options[document.form1.menu.selectedIndex].value;">
+<option value=''>English</option>
+<option value="../Suomi/Ohjelma.php">Suomi</option>
+<!--<option value="../Englanti/Ohjelma.php">English</option>-->
+
+</select>
+</form>
+</div>
+<div class="sidebar-element">
+<h3>Links</h3>
+	<ul>
+		
+		<li><a href="http://www.lomavietonen.com">Holiday Loma Vietonen</a></li>
+		<li><a href="http://www.norwegian.com/uk/?gclid=CM3hrdSCvbsCFYJP3godYh8AtA">Norwegian</a></li>
+		<li><a href="http://www.finnair.com/GB/GB/company-travel">Finnair</a></li>
+		<li><a href="http://www.vr.fi/en/index.html">Railways</a></li>
+		<li><a href="http://www.onnibus.com/en/index.htm">Onnibus, cheap intercity busline</a></li>
+		<li><a href="http://www.foreca.com/Finland/Rovaniemi?tenday">Foreca weather forecast - Rovaniemi</a></li>
+		<li><a href="http://www.foreca.com/Finland/Ylitornio?tenday">Foreca weather forecast - Ylitornio</a></li>
+		<li><a href="http://en.ilmatieteenlaitos.fi/weather/Rovaniemi">The Finnish Meteorological Institute - Rovaniemi</a></li>
+		<li><a href="http://en.ilmatieteenlaitos.fi/weather/Ylitornio">The Finnish Meteorological Institute - Ylitornio</a></li>
+		<li><a href="https://en.wikipedia.org/wiki/Pierre_Louis_Maupertuis">Pierre Louis Moreau de Maupertuis</a></li>
+		<li><a href="https://en.wikipedia.org/wiki/Struve_Geodetic_Arc">Struve Geodetic Arc</a></li>
+		<li><a href="http://www.kukkolankoski.fi/">Kukkolankoski</a></li>
+		<li><a href="http://www.nordicsafaris.com/summer-autumn/rafting-in-kukkolaraft.html">Rafting at Kukkolaraft, only for the bravest</a></li>
+		<li><a href="http://www.hiidenkirnut.com/?lang=2">Hiidenkirnut</a></li>
+		<li><a href="http://aavasaksa.fi/?lang=en">Aavasaksa</a></li>
+		<li><a href="http://lovikka.com/en/">Lovikka</a></li>
+		<li><a href="http://www.santaclausvillage.info/">Arctic Circle</a></li>
+			
+	</ul>
+</div>
+</div>
+
+ 
+
+ 
+
+ 
+
+ 
+
+
+
+ 
+
+</div>
+</div>
+
+<div align=center id="footer">
+<br />
+<img src="../images/mensa-logo.png" alt="" /></a>
+<h3>Mensa Arctic Circle</h3>
+
+</div>
+
+</body>
+
+</html>
